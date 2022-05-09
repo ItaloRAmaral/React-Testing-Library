b46497e26f5284e5ba404e028facfa386f467eaf
Italo Amaral <italorockamaral@gmail.com>
2022-05-09 15:29:09 -0300

    First Commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+describe('Teste o componente <App.js />', () => {
+  it('Verifique se o topo da aplicação contém um conjunto fixo de links de navegação', () => {});
+  it('Verifique se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.', () => {});
+  it('Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.', () => {});
+  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.', () => {});
+  it('Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.', () => {});
+  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {});
+});
